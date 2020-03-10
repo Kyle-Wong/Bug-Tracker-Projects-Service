d3541d32e65450b933732660f74587b58306d558
Kyle Wong <kylewonga@gmail.com>
2020-03-09 18:51:01 -0700

    More detailed project get methods

--- index.js
+++ index.js
@@ -1,173 +1,239 @@
-const express = require('express')
-const util = require('util');
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
+const express = require("express");
+const util = require("util");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const app = express();
 
-const fs = require('fs');
-const logger = require('./logger');
-const ResponseBuilder = require('./response-builder');
+const fs = require("fs");
+const logger = require("./logger");
+const ResponseBuilder = require("./response-builder");
 
-const config = require('config');
-const dbConfig = config.get('dbConfig');
+const config = require("config");
+const dbConfig = config.get("dbConfig");
 logger.log(dbConfig);
-const serverConfig = config.get('serverConfig');
+const serverConfig = config.get("serverConfig");
 logger.log(serverConfig);
 var mysql = require("mysql");
 
-
-app.use(express.static('public'))
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-const project = require('./project');
-const tags = require('./tags');
-const bugs = require('./bugs');
-const errors = require('./errors');
+const project = require("./project");
+const tags = require("./tags");
+const bugs = require("./bugs");
+const errors = require("./errors");
 
+app.get("/", function(req, res) {
+  responseBuilder = new ResponseBuilder(res);
+});
 
-
-app.get('/', function(req, res){
-    responseBuilder = new ResponseBuilder(res);
-    
-})
-
-var server = app.listen(serverConfig.port, function(){
+var server = app.listen(serverConfig.port, function() {
   var host = server.address().address;
   var port = server.address().port;
   logger.log(`Server listening at ${host}:${port}`);
-})
+});
 
-
-app.post('/prjt/user/add', function(req,res){
+app.post("/prjt/user/add", function(req, res) {
   logger.log("Adding user");
   var body = req.body;
   logger.log(body);
-  project.addUser(new ResponseBuilder(res),body.username,body.email,body.is_active,body.account_type);
+  project.addUser(
+    new ResponseBuilder(res),
+    body.username,
+    body.email,
+    body.is_active,
+    body.account_type
+  );
 });
 
-
 //Project
-app.post('/prjt/project/add', function(req,res){
-  logger.log("Create Project")
+app.post("/prjt/project/add", function(req, res) {
+  logger.log("Create Project");
   var body = req.body;
   var headers = req.headers;
   logger.log(req.body);
-  project.createProject(new ResponseBuilder(res),headers.username,body.project_name);
+  project.createProject(
+    new ResponseBuilder(res),
+    headers.username,
+    body.project_name
+  );
 });
 
-app.post('/prjt/project/delete', async function(req,res){
+app.post("/prjt/project/delete", async function(req, res) {
   logger.log("Deleting project");
   var body = req.body;
   var headers = req.headers;
   logger.log(req.body);
-  const hasProjectAccess = await project.verifyAccessLevel(headers.username,body.project_id, project.accessLevel.root);
-  if(hasProjectAccess)
-    project.deleteProject(new ResponseBuilder(res),headers.username,body.project_id);
-  else{
+  const hasProjectAccess = await project.verifyAccessLevel(
+    headers.username,
+    body.project_id,
+    project.accessLevel.root
+  );
+  if (hasProjectAccess)
+    project.deleteProject(
+      new ResponseBuilder(res),
+      headers.username,
+      body.project_id
+    );
+  else {
     new ResponseBuilder(res).default(errors.INSUFFICIENT_ACCESS).end();
   }
 });
-
-app.get('/prjt/project/getAll', async function(req,res){
+app.get("/prjt/project/get/:project_id", async function(req, res) {
+  logger.log("Getting project");
+  var headers = req.headers;
+  var params = req.params;
+  const hasProjectAccess = await project.verifyAccessLevel(
+    headers.username,
+    params.project_id,
+    project.accessLevel.view
+  );
+  if (hasProjectAccess) {
+    project.getProject(new ResponseBuilder(res), params.project_id);
+  } else {
+    new ResponseBuilder(res).default(errors.INSUFFICIENT_ACCESS).end();
+  }
+});
+app.get("/prjt/project/getAll", async function(req, res) {
   logger.log("Getting project list");
   var headers = req.headers;
-  project.getProjectList(new ResponseBuilder(res),headers.username);
+  project.getDetailedProjectList(new ResponseBuilder(res), headers.username);
 });
 
 //Tags
-app.get('/prjt/tag/getAll', function(req,res){
+app.get("/prjt/tag/getAll", function(req, res) {
   logger.log("Get all tags");
   tags.getAllTags(new ResponseBuilder(res));
 });
 
-app.post('/prjt/tag/add', function(req,res){
+app.post("/prjt/tag/add", function(req, res) {
   logger.log("Create tag");
   var body = req.body;
   logger.log(body);
-  tags.createTag(new ResponseBuilder(res),body.tag_name);
+  tags.createTag(new ResponseBuilder(res), body.tag_name);
 });
 
-app.post("/prjt/tag/remove", async function(req,res){
+app.post("/prjt/tag/remove", async function(req, res) {
   logger.log("Removing tags");
   var body = req.body;
   logger.log(body);
-  const hasProjectAccess = await project.verifyAccessLevel(headers.username,body.project_id, project.accessLevel.edit);
-  if(hasProjectAccess)
-    bugs.removeTagsFromBug(new ResponseBuilder(res),body.bug_id,body.tag_names);
-  else{
+  const hasProjectAccess = await project.verifyAccessLevel(
+    headers.username,
+    body.project_id,
+    project.accessLevel.edit
+  );
+  if (hasProjectAccess)
+    bugs.removeTagsFromBug(
+      new ResponseBuilder(res),
+      body.bug_id,
+      body.tag_names
+    );
+  else {
     new ResponseBuilder(res).default(errors.INSUFFICIENT_ACCESS).end();
   }
 });
 
 //bugs
-app.post('/prjt/bug/add', async function(req,res){
+app.post("/prjt/bug/add", async function(req, res) {
   logger.log("Adding bug");
 
   var body = req.body;
   var headers = req.headers;
   logger.log(body);
-  const hasProjectAccess = await project.verifyAccessLevel(headers.username,body.project_id, project.accessLevel.edit);
-  if(hasProjectAccess){
-    bugs.addBug(new ResponseBuilder(res),headers.username,body.project_id,body.title,body.body,body.priority,body.tags);
-  }else{
+  const hasProjectAccess = await project.verifyAccessLevel(
+    headers.username,
+    body.project_id,
+    project.accessLevel.edit
+  );
+  if (hasProjectAccess) {
+    bugs.addBug(
+      new ResponseBuilder(res),
+      headers.username,
+      body.project_id,
+      body.title,
+      body.body,
+      body.priority,
+      body.tags
+    );
+  } else {
     new ResponseBuilder(res).default(errors.INSUFFICIENT_ACCESS).end();
   }
 });
 
-app.post('/prjt/bug/delete', async function(req, res){
+app.post("/prjt/bug/delete", async function(req, res) {
   logger.log("Deleting bug");
   var body = req.body;
   var headers = req.headers;
   logger.log(body);
-  const validAccessLevel = await project.verifyAccessLevel(headers.username,body.project_id,project.accessLevel.edit);
-  if(validAccessLevel){
+  const validAccessLevel = await project.verifyAccessLevel(
+    headers.username,
+    body.project_id,
+    project.accessLevel.edit
+  );
+  if (validAccessLevel) {
     bugs.deleteBug(new ResponseBuilder(res), body.bug_id);
   } else {
     new ResponseBuilder(res).default(errors.INSUFFICIENT_ACCESS).end();
   }
 });
 
-app.post('/prjt/bug/resolve', async function(req, res){
+app.post("/prjt/bug/resolve", async function(req, res) {
   logger.log("Resolving bug");
   var body = req.body;
   var headers = req.headers;
   logger.log(body);
-  const validAccessLevel = await project.verifyAccessLevel(headers.username,body.project_id,project.accessLevel.edit);
-  if(validAccessLevel){
-    bugs.resolveBug(new ResponseBuilder(res),body.bug_id);
+  const validAccessLevel = await project.verifyAccessLevel(
+    headers.username,
+    body.project_id,
+    project.accessLevel.edit
+  );
+  if (validAccessLevel) {
+    bugs.resolveBug(new ResponseBuilder(res), body.bug_id);
   } else {
     new ResponseBuilder(res).default(errors.INSUFFICIENT_ACCESS).end();
   }
 });
 
-app.post('/prjt/bug/assign', async function(req,res){
+app.post("/prjt/bug/assign", async function(req, res) {
   logger.log("assigning bug to user");
   var body = req.body;
   var headers = req.headers;
   logger.log(body);
-  const validAccessLevel = await project.verifyAccessLevel(headers.username,body.project_id,project.accessLevel.edit);
-  if(validAccessLevel){
-    bugs.assignBugToUser(new ResponseBuilder(res),headers.username,body.bug_id);
+  const validAccessLevel = await project.verifyAccessLevel(
+    headers.username,
+    body.project_id,
+    project.accessLevel.edit
+  );
+  if (validAccessLevel) {
+    bugs.assignBugToUser(
+      new ResponseBuilder(res),
+      headers.username,
+      body.bug_id
+    );
   } else {
     new ResponseBuilder(res).default(errors.INSUFFICIENT_ACCESS).end();
   }
 });
 
-app.post('/prjt/bug/unassign', async function(req, res){
+app.post("/prjt/bug/unassign", async function(req, res) {
   logger.log("Removing bug from user");
   var body = req.body;
   var headers = req.headers;
   logger.log(body);
-  const validAccessLevel = await project.verifyAccessLevel(headers.username,body.project_id,project.accessLevel.edit);
-  if(validAccessLevel){
-    bugs.removeBugFromUser(new ResponseBuilder(res),body.bug_id);
+  const validAccessLevel = await project.verifyAccessLevel(
+    headers.username,
+    body.project_id,
+    project.accessLevel.edit
+  );
+  if (validAccessLevel) {
+    bugs.removeBugFromUser(new ResponseBuilder(res), body.bug_id);
   } else {
     new ResponseBuilder(res).default(errors.INSUFFICIENT_ACCESS).end();
   }
 });
 
-app.post("/prjt/bug/get", async function(req, res){
+app.post("/prjt/bug/get", async function(req, res) {
   /*
     body: project_id, tags_filter
     query: search string, page num, result order, order direction, include resolved
@@ -176,48 +242,69 @@ app.post("/prjt/bug/get", async function(req, res){
   var body = req.body;
   var headers = req.headers;
   var query = req.query;
-  
+
   const page = parseInt(query.page);
-  if(Number.isNaN(page)){
+  if (Number.isNaN(page)) {
     page = 0;
   }
-  if(!["title","create_time","priority"].includes(query.order.toLowerCase())){
-    query.order = "title"
+  if (
+    !["title", "create_time", "priority"].includes(query.order.toLowerCase())
+  ) {
+    query.order = "title";
   }
-  if(!["asc","desc"].includes(query.direction.toLowerCase())){
-    query.direction = "desc"
+  if (!["asc", "desc"].includes(query.direction.toLowerCase())) {
+    query.direction = "desc";
   }
-  if(!["true","false"].includes(query.includeResolved.toLowerCase())){
+  if (!["true", "false"].includes(query.includeResolved.toLowerCase())) {
     query.includeResolved = "true";
   }
   logger.log(body);
   logger.log(query);
-  bugs.getBugList(new ResponseBuilder(res),body.project_id,query.search,query.page,
-    query.order,query.direction,query.includeResolved,body.tags_filter);
-
+  bugs.getBugList(
+    new ResponseBuilder(res),
+    body.project_id,
+    query.search,
+    query.page,
+    query.order,
+    query.direction,
+    query.includeResolved,
+    body.tags_filter
+  );
 });
 
 //invitations
-app.get('/prjt/invite/getAll', async function(req,res){
+app.get("/prjt/invite/getAll", async function(req, res) {
   logger.log("Getting invitation list");
   var headers = req.headers;
-  project.getInvitationList(new ResponseBuilder(res),headers.username);
+  project.getInvitationList(new ResponseBuilder(res), headers.username);
 });
 
-app.post('/prjt/invite/send', async function(req,res){
+app.post("/prjt/invite/send", async function(req, res) {
   logger.log("Inviting user to project");
   var body = req.body;
   var headers = req.headers;
   logger.log(body);
-  const validAccessLevel = await project.verifyAccessLevel(headers.username,body.project_id,project.accessLevel.root);
-  const alreadyHasAccess = await project.verifyAccessLevel(body.invited,body.project_id,2);
-  if(validAccessLevel){
-    if(!alreadyHasAccess)
-      project.inviteUser(new ResponseBuilder(res),body.invited,body.project_id,body.access_level);
-    else{
+  const validAccessLevel = await project.verifyAccessLevel(
+    headers.username,
+    body.project_id,
+    project.accessLevel.root
+  );
+  const alreadyHasAccess = await project.verifyAccessLevel(
+    body.invited,
+    body.project_id,
+    2
+  );
+  if (validAccessLevel) {
+    if (!alreadyHasAccess)
+      project.inviteUser(
+        new ResponseBuilder(res),
+        body.invited,
+        body.project_id,
+        body.access_level
+      );
+    else {
       logger.log(`User(${body.invited}) already has access`);
       new ResponseBuilder(res).default(errors.USER_ALREADY_ACCESSED).end();
-
     }
   } else {
     //inviter isn't root user (can't invite) or invited already has access
@@ -225,16 +312,21 @@ app.post('/prjt/invite/send', async function(req,res){
   }
 });
 
-app.post('/prjt/invite/resolve', async function(req,res){
+app.post("/prjt/invite/resolve", async function(req, res) {
   logger.log("Resolving invitation");
   var body = req.body;
   var headers = req.headers;
   var query = req.query;
   var accepted;
-  if(query.accepted === 'true'){
+  if (query.accepted === "true") {
     accepted = true;
   } else {
     accepted = false;
   }
-  project.resolveInvitation(new ResponseBuilder(res),headers.username,body.project_id,accepted);
+  project.resolveInvitation(
+    new ResponseBuilder(res),
+    headers.username,
+    body.project_id,
+    accepted
+  );
 });
--- project.js
+++ project.js
@@ -1,172 +1,230 @@
-const errors = require('./errors');
-const logger = require('./logger');
+const errors = require("./errors");
+const logger = require("./logger");
 const pool = require("./database");
 exports.accessLevel = {
-    root:0,
-    edit:1,
-    view:2,
-}
+  root: 0,
+  edit: 1,
+  view: 2
+};
 
+exports.createProject = async function(resBuilder, rootUser, projectName) {
+  var errorCode = errors.validUsername(rootUser);
+  if (errorCode != 0) {
+    resBuilder.default(errorCode).end();
+  }
+  errorCode = errors.validProjectName(projectName);
+  if (errorCode != 0) {
+    resBuilder.default(errorCode).end();
+  }
 
-
-exports.createProject = async function(resBuilder, rootUser, projectName){
-    var errorCode = errors.validUsername(rootUser);
-    if(errorCode != 0){
-        resBuilder.default(errorCode).end();
-    }
-    errorCode = errors.validProjectName(projectName);
-    if(errorCode != 0){
-        resBuilder.default(errorCode).end();
-    }
-
-    var query = `INSERT INTO projects(root_user,project_name,is_deleted) VALUES (?,?,?); 
+  var query = `INSERT INTO projects(root_user,project_name,is_deleted) VALUES (?,?,?); 
     SELECT LAST_INSERT_ID() as project_id;`;
-    try{
-        const rows = await pool.query(query,[rootUser,projectName,0]);
-        const projectID = rows[1][0].project_id;
-        changeProjectAccess(rootUser,projectID,exports.accessLevel.root);
-        resBuilder.success().end();
-    } catch(err){
-        logger.sqlErr(err);
-        if(err.errno == 1452){
-            //user doesn't exist
-            resBuilder.default(405).end();
-        } else {
-            resBuilder.error(err).end();
-        }
+  try {
+    const rows = await pool.query(query, [rootUser, projectName, 0]);
+    const projectID = rows[1][0].project_id;
+    changeProjectAccess(rootUser, projectID, exports.accessLevel.root);
+    resBuilder.success().end();
+  } catch (err) {
+    logger.sqlErr(err);
+    if (err.errno == 1452) {
+      //user doesn't exist
+      resBuilder.default(405).end();
+    } else {
+      resBuilder.error(err).end();
     }
-    
+  }
+};
+exports.allowProjectAccess = async function(
+  resBuilder,
+  username,
+  projectID,
+  accessLevel
+) {
+  var errorCode = errors.validUsername(username);
+  if (errorCode != 0) {
+    resBuilder.default(errorCode).end();
+  }
 
-}
-exports.allowProjectAccess = async function(resBuilder, username, projectID, accessLevel){
-    var errorCode = errors.validUsername(username);
-    if(errorCode != 0){
-        resBuilder.default(errorCode).end();
-    }
+  var success = changeProjectAccess(username, projectID, accessLevel);
+  if (success) resBuilder.success().end();
+  else resBuilder.default(400).end();
+};
 
-    var success = changeProjectAccess(username,projectID,accessLevel);
-    if(success)
-        resBuilder.success().end();
-    else
-        resBuilder.default(400).end();
-}
+exports.addUser = async function(
+  resBuilder,
+  username,
+  email,
+  isActive,
+  accountType
+) {
+  logger.log(`Registering ${username} to db`);
+  var query = `INSERT INTO users VALUES(?,?,?,?)`;
+  try {
+    await pool.query(query, [username, email, isActive, accountType]);
+    return resBuilder.success().end();
+  } catch (err) {
+    logger.log(err);
+    return resBuilder.error(err).end();
+  }
+};
 
-
-exports.addUser = async function(resBuilder, username, email, isActive, accountType){
-    logger.log(`Registering ${username} to db`);
-    var query = `INSERT INTO users VALUES(?,?,?,?)`
-    try{
-        await pool.query(query,[username,email,isActive,accountType]);
-        return resBuilder.success().end();
-    } catch(err){
-        logger.log(err);
-        return resBuilder.error(err).end();
-    }
-}
-
-async function changeProjectAccess(username, projectID, accessLevel){
-    var query = `INSERT INTO project_access VALUES (?,?,?)
+async function changeProjectAccess(username, projectID, accessLevel) {
+  var query = `INSERT INTO project_access VALUES (?,?,?)
                 ON DUPLICATE KEY UPDATE access_level=?`;
-    try{
-        await pool.query(query,[username,projectID,accessLevel,accessLevel]);
-        return true;
-    } catch(err){
-        logger.log(err);
-        return false;
-    }
+  try {
+    await pool.query(query, [username, projectID, accessLevel, accessLevel]);
+    return true;
+  } catch (err) {
+    logger.log(err);
+    return false;
+  }
 }
 
-exports.verifyAccessLevel = async function(username,projectID,requiredAccess){
-    logger.log(`Checking access level for ${username}--required=${requiredAccess}`);
-    if(username == null){
-        logger.log("username is null");
-        return false;
-    }
-    var query = `SELECT username, access_level 
+exports.verifyAccessLevel = async function(
+  username,
+  projectID,
+  requiredAccess
+) {
+  logger.log(
+    `Checking access level for ${username}--required=${requiredAccess}`
+  );
+  if (username == null) {
+    logger.log("username is null");
+    return false;
+  }
+  var query = `SELECT username, access_level 
             FROM project_access pa 
-            WHERE username = ? AND project_id = ?;`
-    try{
-        const rows = await pool.query(query,[username,projectID]);
-        console.log(rows);
-        if(rows.length == 0){
-            //user has no access record for the project at all
-            logger.log("Insufficient Access--Undefined")
-            return false;
-        }
-        if(rows[0].access_level > requiredAccess){
-            //user's access is insufficient
-            logger.log(`Insufficient Access--${rows[0].access_level} (${requiredAccess} required)`);
-            return false
-        }
-        //user has sufficient access to project
-        logger.log("Sufficient Access");
-        return true;
-    } catch(err){
-        logger.sqlErr(err);
-        return false;
+            WHERE username = ? AND project_id = ?;`;
+  try {
+    const rows = await pool.query(query, [username, projectID]);
+    console.log(rows);
+    if (rows.length == 0) {
+      //user has no access record for the project at all
+      logger.log("Insufficient Access--Undefined");
+      return false;
     }
-}
+    if (rows[0].access_level > requiredAccess) {
+      //user's access is insufficient
+      logger.log(
+        `Insufficient Access--${rows[0].access_level} (${requiredAccess} required)`
+      );
+      return false;
+    }
+    //user has sufficient access to project
+    logger.log("Sufficient Access");
+    return true;
+  } catch (err) {
+    logger.sqlErr(err);
+    return false;
+  }
+};
 
-exports.getProjectList = async function(resBuilder,username){
-
-    var query = `SELECT * FROM projects WHERE root_user=? AND is_deleted = 0 ORDER BY project_name`;
-    try{
-        const rows = await pool.query(query,[username]);
-        resBuilder.json["projects"] = rows;
-        logger.log(rows);
-        return resBuilder.success().end();
-    } catch(err){
-        logger.sqlErr(err);
-        return resBuilder.error(err).end();
-    }
-}
-exports.deleteProject = async function(resBuilder,username,projectID){
-    var query = `UPDATE projects SET is_deleted=1 WHERE projectID=? AND root_user=?`;
-    try{
-        const rows = await pool.query(query,[username,projectID]);
-        logger.log(rows);
-        return resBuilder.success().end();
-    } catch(err){
-        logger.sqlErr(err);
-        return resBuilder.error(err).end();
-    }
-}
-exports.getInvitationList = async function(resBuilder,username){
-    var query = `SELECT p.project_id, project_name, root_user, access_level
+exports.getProjectList = async function(resBuilder, username) {
+  var query = `SELECT * FROM projects WHERE root_user=? AND is_deleted = 0 ORDER BY project_name`;
+  try {
+    const rows = await pool.query(query, [username]);
+    resBuilder.json["projects"] = rows;
+    logger.log(rows);
+    return resBuilder.success().end();
+  } catch (err) {
+    logger.sqlErr(err);
+    return resBuilder.error(err).end();
+  }
+};
+exports.getDetailedProjectList = async function(resBuilder, username) {
+  var query = `SELECT * FROM 
+              (SELECT p.project_id,p.root_user,p.project_name,p.is_deleted, COUNT(p.project_id) as active_bugs 
+              FROM bugs b, projects p
+              WHERE p.project_id = b.project_id AND
+              b.project_id IN (SELECT project_id from project_access) 
+              AND p.is_deleted = 0
+              GROUP BY p.project_id) p,
+              project_access pa
+              WHERE pa.project_id = p.project_id AND pa.username=?;`;
+  try {
+    const rows = await pool.query(query, [username]);
+    resBuilder.json["projects"] = rows;
+    logger.log(rows);
+    return resBuilder.success().end();
+  } catch (err) {
+    logger.sqlErr(err);
+    return resBuilder.error(err).end();
+  }
+};
+exports.getProject = async function(resBuilder, project_id) {
+  var query = `SELECT * FROM projects WHERE project_id=? AND is_deleted = 0`;
+  try {
+    const rows = await pool.query(query, [project_id]);
+    resBuilder.json["project"] = rows;
+    logger.log(rows);
+    return resBuilder.success().end();
+  } catch (err) {
+    logger.sqlErr(err);
+    return resBuilder.error(err).end();
+  }
+};
+exports.deleteProject = async function(resBuilder, username, projectID) {
+  var query = `UPDATE projects SET is_deleted=1 WHERE projectID=? AND root_user=?`;
+  try {
+    const rows = await pool.query(query, [username, projectID]);
+    logger.log(rows);
+    return resBuilder.success().end();
+  } catch (err) {
+    logger.sqlErr(err);
+    return resBuilder.error(err).end();
+  }
+};
+exports.getInvitationList = async function(resBuilder, username) {
+  var query = `SELECT p.project_id, project_name, root_user, access_level
                 FROM invitations i, projects p
                 WHERE p.project_id = i.project_id
                 AND p.is_deleted=0 AND i.invited=?;`;
-    try{
-        const rows = await pool.query(query,[username]);
-        resBuilder.json["invitations"] = rows;
-        return resBuilder.success().end();
-    } catch(err){
-        logger.sqlErr(err).end();
-        return resBuilder.error(err).end();
-    }
-}
-exports.inviteUser = async function(resBuilder,invited,projectID, accessLevel){
-    var query = `INSERT INTO invitations VALUES(?,?,?);`
-    try{
-        const rows = await pool.query(query,[invited,projectID,accessLevel]);
-        return resBuilder.success().end();
-    } catch(err){
-        logger.sqlErr(err).end();
-        return resBuilder.error(err).end();
-    }
-}
-exports.resolveInvitation = async function(resBuilder, username, projectID, accepted){
-    var query = `SELECT * FROM invitations WHERE invited=? AND project_id=?;
+  try {
+    const rows = await pool.query(query, [username]);
+    resBuilder.json["invitations"] = rows;
+    return resBuilder.success().end();
+  } catch (err) {
+    logger.sqlErr(err).end();
+    return resBuilder.error(err).end();
+  }
+};
+exports.inviteUser = async function(
+  resBuilder,
+  invited,
+  projectID,
+  accessLevel
+) {
+  var query = `INSERT INTO invitations VALUES(?,?,?);`;
+  try {
+    const rows = await pool.query(query, [invited, projectID, accessLevel]);
+    return resBuilder.success().end();
+  } catch (err) {
+    logger.sqlErr(err).end();
+    return resBuilder.error(err).end();
+  }
+};
+exports.resolveInvitation = async function(
+  resBuilder,
+  username,
+  projectID,
+  accepted
+) {
+  var query = `SELECT * FROM invitations WHERE invited=? AND project_id=?;
                 DELETE FROM invitations WHERE invited=? AND project_id=?;`;
-    try{
-        const rows = await pool.query(query,[username,projectID,username,projectID]);
-        logger.log(rows);
-        if(accepted){
-            await changeProjectAccess(username,projectID,rows[0][0].access_level);
-        }
-        return resBuilder.success().end();
-    } catch(err){
-        logger.sqlErr(err);
-        return resBuilder.error(err).end();
+  try {
+    const rows = await pool.query(query, [
+      username,
+      projectID,
+      username,
+      projectID
+    ]);
+    logger.log(rows);
+    if (accepted) {
+      await changeProjectAccess(username, projectID, rows[0][0].access_level);
     }
-}
+    return resBuilder.success().end();
+  } catch (err) {
+    logger.sqlErr(err);
+    return resBuilder.error(err).end();
+  }
+};
